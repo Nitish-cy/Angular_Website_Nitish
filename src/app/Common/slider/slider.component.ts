5e34e09d7aeb41db0c937d94e55c4f332b003076
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2023-01-23 17:07:43 +0530

    New Changes

--- src/app/Common/slider/slider.component.ts
+++ src/app/Common/slider/slider.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+//import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 @Component({
-  selector: 'app-slider',
+  selector: 'app-slider,ngb-carousel',
   templateUrl: './slider.component.html',
+ 
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
